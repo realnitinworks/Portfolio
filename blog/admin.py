303aec80b741cdb8177c509971a67b3804d3631e
realnitinworks <realnitinworks@gmail.com>
2020-05-17 00:45:25 +0530

    Add blog app and DB Schema for Post.

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    readonly_fields = ('created', 'updated')
+    prepopulated_fields = {'slug': ('title', )}
