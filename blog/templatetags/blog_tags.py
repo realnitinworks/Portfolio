bab557fcd2e580e711c7251d49736a15a2048d0b
realnitinworks <realnitinworks@gmail.com>
2020-05-24 13:08:28 +0530

    Use Mistune for markdown rendering and syntax highlight.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,12 +1,28 @@
 from django import template
 from django.utils.safestring import mark_safe
 
-import markdown
+import mistune
+from mistune.directives import DirectiveToc
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
 
 register = template.Library()
 
 
-@register.filter(name='markdown')
+class HighlightRenderer(mistune.HTMLRenderer):
+    def block_code(self, code, lang=None):
+        if lang:
+            lexer = get_lexer_by_name(lang, stripall=True)
+            formatter = html.HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        return f'<pre><code>{mistune.escape(code)}</code></pre>'
+
+
+@register.filter(name="markdown")
 def markdown_format(text):
-    return mark_safe(markdown.markdown(text))
+    plugins = [DirectiveToc(), 'url', 'footnotes']
+    renderer = HighlightRenderer()
+    markdown = mistune.create_markdown(renderer=renderer, plugins=plugins)
+    return mark_safe(markdown(text))
